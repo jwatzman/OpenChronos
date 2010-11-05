345e3734b912acf86f8314763da24d8ca829852f
Josh Watzman <jwatzman@jwatzman.org>
2010-11-04 21:53:55 -0400

    Clean up prout

--- logic/prout.c
+++ logic/prout.c
@@ -1,19 +1,19 @@
 /*
-    Vario function for ez430 chronos watch.
-    Copyright (C) 2010 Marc Poulhiès <dkm@kataplop.net>
+   Vario function for ez430 chronos watch.
+   Copyright (C) 2010 Marc Poulhiès <dkm@kataplop.net>
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 // *************************************************************************************************
@@ -78,62 +78,45 @@ static u8 *str = POUET_STR;
 
 void prout_tick()
 {
-  sprouttimer.pos = (sprouttimer.pos+1) % (sizeof(POUET_STR)-7);
-  display_prout(0, 0);
+	sprouttimer.pos = (sprouttimer.pos+1) % (sizeof(POUET_STR)-7);
+	display_prout(0, 0);
 }
 
 u8 is_prout(void)
 {
-  return (sprouttimer.state == PROUT_RUN &&  (ptrMenu_L2 == &menu_L2_Prout));
+	return (sprouttimer.state == PROUT_RUN &&  (ptrMenu_L2 == &menu_L2_Prout));
 }
 
 void update_prout_timer()
 {
-  /* TA0CCR2 = TA0CCR2 + STOPWATCH_1HZ_TICK; */
 }
 
 void start_prout()
 {
-
-  sprouttimer.state = PROUT_RUN;
-  /* // Init CCR register with current time */
-  /* TA0CCR2 = TA0R; */
-		
-  /* // Load CCR register with next capture time */
-  /* update_prout_timer(); */
-
-  /* // Reset IRQ flag     */
-  /* TA0CCTL2 &= ~CCIFG;  */
-	          
-  /* // Enable timer interrupt     */
-  /* TA0CCTL2 |= CCIE;  */
-	
-  display_symbol(LCD_ICON_RECORD, SEG_ON);
+	sprouttimer.state = PROUT_RUN;
+	display_symbol(LCD_ICON_RECORD, SEG_ON);
 }
 
 void stop_prout()
 {
-  /* // Clear timer interrupt enable    */
-  /* TA0CCTL2 &= ~CCIE;  */
-
 	reset_prout();
-	
-  display_symbol(LCD_ICON_RECORD, SEG_OFF);
 
-  // Call draw routine immediately
-  display_prout(LINE2, DISPLAY_LINE_UPDATE_FULL);
+	display_symbol(LCD_ICON_RECORD, SEG_OFF);
+
+	// Call draw routine immediately
+	display_prout(LINE2, DISPLAY_LINE_UPDATE_FULL);
 }
 
 void sx_prout(u8 line)
 {
-  if (button.flag.down)
-    {
-      if (sprouttimer.state == PROUT_STOP){
-        start_prout();
-      } else {
-        stop_prout();
-      }
-    }
+	if (button.flag.down)
+	{
+		if (sprouttimer.state == PROUT_STOP){
+			start_prout();
+		} else {
+			stop_prout();
+		}
+	}
 }
 
 void mx_prout(u8 line)
@@ -142,17 +125,17 @@ void mx_prout(u8 line)
 
 void display_prout(u8 line, u8 update)
 {
-  u8 cur[7];
-  memcpy(cur, str + sprouttimer.pos, 6);
-  cur[6] = 0;
-  
-  display_chars(LCD_SEG_L2_5_0, cur, SEG_ON);
+	u8 cur[7];
+	memcpy(cur, str + sprouttimer.pos, 6);
+	cur[6] = 0;
+
+	display_chars(LCD_SEG_L2_5_0, cur, SEG_ON);
 }
 
 void reset_prout(void)
 {
-  sprouttimer.pos = 0;
-  sprouttimer.state = PROUT_STOP;
+	sprouttimer.pos = 0;
+	sprouttimer.state = PROUT_STOP;
 }
 
 #endif /* CONFIG_PROUT */
